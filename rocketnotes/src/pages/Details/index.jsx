81279e85147b3d3c703a8b9b0f534a79514388ce
Victor Miguel Vilela Fernandes <113549997+victormiguelVF@users.noreply.github.com>
2023-01-10 18:00:50 -0300

    Continuando projeto-rocketnotes e estudos de React

--- rocketnotes/src/pages/Details/index.jsx
+++ rocketnotes/src/pages/Details/index.jsx
@@ -1,16 +1,45 @@
-import { Container } from './styles';
+import { Container, Links, Content } from './styles';
 
+import { Tag } from '../../components/Tag';
 import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
+
 
 export function Details(){
+
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Victor Miguel</span>
+      <Header />
 
-      <Button title="Login" loading />
-      <Button title="Cadastrar" />
-      <Button title="Voltar" />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>
+            Introdução ao React
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus qui dolore, tempore quae expedita vero omnis eum ratione sed saepe unde sunt placeat! Assumenda perferendis nostrum mollitia non quas? Voluptate?
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://rocketseat.com.br/</a></li>
+              <li><a href="#">https://rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
